34d76b8934e165eb09c6126aff60d2c9cca29256
Vincent Jodjana <vcjod00@gmail.com>
2019-11-22 10:38:03 -0800

    Finished program

---
+++ cars.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+#cars.sh
+#Vincent Jodjana
+
+clear
+continue=true
+
+while $continue
+do	
+	echo Type the number one to enter a new car
+	echo Type the number two to display a list of cars
+	echo Type the number three to quit and exit the program
+	read -r input
+
+	case $input in
+		1)
+			clear
+			echo "Enter Year"
+			read -r year
+			echo "Enter Make"
+			read -r make
+			echo "Enter Model"
+			read -r model
+			car=$year":$make:"$model
+			echo "$car" >> My_Old_Cars
+			sort -o My_Old_Cars My_Old_Cars
+			clear
+			;;
+		2)
+			clear
+			cat My_Old_Cars
+			echo
+			echo
+			;;
+		3)
+			continue=false
+			clear
+			;;
+	esac
+done
+					
